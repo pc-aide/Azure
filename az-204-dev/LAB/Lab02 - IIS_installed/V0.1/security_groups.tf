82ae0bbaa936990d92a08a951fd84dbf2ca1c05d
pc-aide <36453201+pc-aide@users.noreply.github.com>
2023-03-04 14:06:33 -0500

    Update security_groups.tf

--- az-204-dev/LAB/Lab02 - IIS_installed/V0.1/security_groups.tf
+++ az-204-dev/LAB/Lab02 - IIS_installed/V0.1/security_groups.tf
@@ -1,11 +1,12 @@
-resource "azurerm_network_security_group" "sg_rdp" {
-  name                = var.sg_rdp
+resource "azurerm_network_security_group" "nsg_nic" {
+  name                = var.nsg_vm
   location            = var.location
   resource_group_name = var.rg_name
 
-  # Rule
+  # Rules
+  # RDP
   security_rule {
-    name                       = var.sec_rule_name
+    name                       = var.sec_rule_name_rdp
     access                     = "Allow"
     description                = "Allow RDP Inbound"
     direction                  = "Inbound"
@@ -14,6 +15,20 @@ resource "azurerm_network_security_group" "sg_rdp" {
     destination_port_range     = "3389"
     priority                   = 100
     source_address_prefix      = var.src_address_prefix
+    source_port_range          = "*"
+  }
+
+  # HTTP
+  security_rule {
+    name                       = var.sec_rule_name_http
+    access                     = "Allow"
+    description                = "Allow HTTP Inbound"
+    direction                  = "Inbound"
+    protocol                   = "Tcp"
+    destination_address_prefix = "*"
+    destination_port_range     = "80"
+    priority                   = 110
+    source_address_prefix      = "*"
     source_port_range          = "*"
   }
 
