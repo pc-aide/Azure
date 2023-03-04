00602fcd209ae61d66ae5b2a91775af014165de3
pc-aide <36453201+pc-aide@users.noreply.github.com>
2023-03-04 18:20:34 -0500

    Create security_groups.tf

---
+++ az-204-dev/LAB/Lab03 - Deploy .NET Core app on 2k19/V0.1/security_groups.tf
@@ -0,0 +1,38 @@
+resource "azurerm_network_security_group" "nsg_nic" {
+  name                = var.nsg_vm
+  location            = var.location
+  resource_group_name = var.rg_lab
+
+  # Rules
+  # RDP
+  security_rule {
+    name                       = var.sec_rule_name_rdp
+    access                     = "Allow"
+    description                = "Allow RDP Inbound"
+    direction                  = "Inbound"
+    protocol                   = "Tcp"
+    destination_address_prefix = "*"
+    destination_port_range     = "3389"
+    priority                   = 100
+    source_address_prefix      = var.src_address_prefix
+    source_port_range          = "*"
+  }
+
+  # HTTP
+  security_rule {
+    name                       = var.sec_rule_name_http
+    access                     = "Allow"
+    description                = "Allow HTTP Inbound"
+    direction                  = "Inbound"
+    protocol                   = "Tcp"
+    destination_address_prefix = "*"
+    destination_port_range     = "80"
+    priority                   = 110
+    source_address_prefix      = "*"
+    source_port_range          = "*"
+  }
+
+  depends_on = [
+    azurerm_resource_group.rg_lab
+  ]
+}
