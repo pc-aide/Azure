2bd004d2c675afc2e7229431bb860a8861aae2b8
pc-aide <36453201+pc-aide@users.noreply.github.com>
2023-03-20 20:42:04 -0400

    Create instance.tf

---
+++ az-204-dev/LAB/Lab04 - Ubu-SRV20.04/V0.1/tf/instance.tf
@@ -0,0 +1,30 @@
+resource "azurerm_linux_virtual_machine" "vm_ubu" {
+  name                = var.vm_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg_lab.name
+  size                = var.vm_size
+
+  # creds
+  admin_username                  = var.adm_usr
+  admin_password                  = var.adm_pwd
+  disable_password_authentication = false
+
+  # nic
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  # OS
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = var.os_disk
+  }
+
+  # image
+  source_image_reference {
+    publisher = var.ima_pub
+    offer     = var.ima_off
+    sku       = var.ima_sku
+    version   = var.ima_ver
+  }
+}
