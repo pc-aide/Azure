6a46ca9c1cba0686630d34a8cb57bdae8c2c22f1
pc-aide <36453201+pc-aide@users.noreply.github.com>
2023-03-21 20:37:58 -0400

    Update user_data.sh

--- az-204-dev/LAB/Lab04 - Ubu-SRV20.04/V0.1/tf/user_data.sh
+++ az-204-dev/LAB/Lab04 - Ubu-SRV20.04/V0.1/tf/user_data.sh
@@ -9,3 +9,12 @@ sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get update
 sudo apt-get install -y apt-transport-https && sudo apt-get update && sudo apt-get install -y aspnetcore-runtime-6.0
 rm packages-microsoft-prod.deb
+
+# stop nginx
+sudo systemctl stop nginx
+
+# test - remplacer default location - Ln48
+sudo sed -i '48,/# First attempt/ {s|# First attempt|        proxy_pass http://localhost:5000;\n        proxy_http_version 1.1;\n        proxy_set_header Upgrade $http_upgrade;\n        proxy_set_header Connection keep-alive;\n        proxy_set_header Host $host;\n        proxy_cache_bypass $http_upgrade;|}' /etc/nginx/sites-available/default
+
+# start nginx
+sudo systemctl start nginx
