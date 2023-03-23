1879ee69cce41b37c5ca7762c5b51106a41b7204
pc-aide <36453201+pc-aide@users.noreply.github.com>
2023-03-22 22:45:39 -0400

    Create commands.sh

---
+++ az-204-dev/LAB/Lab05 - WebApp .NET 6/v0.1/tf/commands.sh
@@ -0,0 +1,27 @@
+# account cleanUp
+az account clear
+
+# creds
+read -p "Your email: " email && az login -u $email
+
+# code format
+terraform fmt -recursive
+
+# init
+terraform init
+
+# validate
+terraform validate
+
+# graph with graphviz (dot)
+terraform graph | dot -Tsvg > graph.svg
+
+# deploy
+terraform apply -auto-approve
+
+# to update resource (eg: vmEntension, nsg)
+# terraform state list (to list resources)
+terraform apply -auto-approve -replace="azurerm_windows_web_app.win_web_app"
+
+# destroy
+terraform destroy -auto-approve
