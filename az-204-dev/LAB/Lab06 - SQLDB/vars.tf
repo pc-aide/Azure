f9ac96b081f3140de4f0119460af51e83d7096da
pc-aide <36453201+pc-aide@users.noreply.github.com>
2023-04-04 20:57:13 -0400

    Create vars.tf

---
+++ az-204-dev/LAB/Lab06 - SQLDB/vars.tf
@@ -0,0 +1,54 @@
+variable "rg_name" {
+  type    = string
+  default = "rg-lab06"
+}
+
+variable "location" {
+  type    = string
+  default = "canadacentral"
+}
+
+# must have a name unique & available
+# *.database.windows.net
+variable "sql_svr_name" {
+  type    = string
+  default = "sql-svr-lab05"
+}
+
+variable "sql_srv_adm" {
+  type        = string
+  default     = "sqladmin"
+  description = "admin_login"
+  sensitive   = true
+}
+
+variable "sql_srv_pwd" {
+  type        = string
+  description = "adm_pwd"
+  sensitive   = true
+
+  validation {
+    condition     = length(var.sql_srv_pwd) >= 8
+    error_message = "Le pwd must be at least 8 characters in length"
+  }
+
+  validation {
+    condition     = length(regexall("[!@#$%^&*(),.?\":{}|<>]", var.sql_srv_pwd)) >= 3
+    error_message = "pwd must containt characters from 3 specials"
+  }
+}
+
+variable "sql_srv_ver" {
+  type    = string
+  default = "12.0"
+}
+
+variable "sql_db_name" {
+  type    = string
+  default = "app-db"
+}
+
+variable "sql_db_sku" {
+  type    = string
+  default = "Basic"
+}
