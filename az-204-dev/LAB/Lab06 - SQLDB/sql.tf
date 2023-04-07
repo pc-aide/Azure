114dfb26b53be55fb22b91af2b5e023d66262dec
pc-aide <36453201+pc-aide@users.noreply.github.com>
2023-04-07 11:33:56 -0400

    Update sql.tf

--- az-204-dev/LAB/Lab06 - SQLDB/sql.tf
+++ az-204-dev/LAB/Lab06 - SQLDB/sql.tf
@@ -3,6 +3,7 @@ resource "random_string" "ran_sql_srv_name_pre" {
   special = false
 }
 
+# sql srv
 resource "azurerm_mssql_server" "sql_srv" {
   name                         = var.sql_svr_name
   resource_group_name          = azurerm_resource_group.rg_lab.name
@@ -13,8 +14,25 @@ resource "azurerm_mssql_server" "sql_srv" {
   version = var.sql_srv_ver
 }
 
+# sql db
 resource "azurerm_mssql_database" "sql_db" {
   name      = var.sql_db_name
   server_id = azurerm_mssql_server.sql_srv.id
   sku_name  = var.sql_db_sku
+}
+
+# sql fw rule : AllowMyISP
+resource "azurerm_mssql_firewall_rule" "sql_fw_rul" {
+  name             = var.sql_fw_rul_isp
+  server_id        = azurerm_mssql_server.sql_srv.id
+  start_ip_address = data.http.ipconfigme.response_body
+  end_ip_address   = data.http.ipconfigme.response_body
+}
+
+# sql fw rule : Allow Azure services and resources to access this server
+resource "azurerm_mssql_firewall_rule" "allow_az_svc" {
+  name             = var.sql_fw_rul_svc
+  server_id        = azurerm_mssql_server.sql_srv.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
 }
