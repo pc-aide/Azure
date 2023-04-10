d5f6687c23a3ad5848477592e94ca6104cb56008
pc-aide <36453201+pc-aide@users.noreply.github.com>
2023-04-10 16:26:39 -0400

    Update providers.tf

--- az-204-dev/LAB/Lab07 - FunctionApp/v0.1/providers.tf
+++ az-204-dev/LAB/Lab07 - FunctionApp/v0.1/providers.tf
@@ -17,6 +17,10 @@ terraform {
 #####################################################
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
   # skip_provider_registration = true
 }
