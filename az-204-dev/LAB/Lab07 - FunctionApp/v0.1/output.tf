1134368e64dd73cb1f62c43ee99144c6dd1ea7aa
pc-aide <36453201+pc-aide@users.noreply.github.com>
2023-04-19 19:44:37 -0400

    Update output.tf

--- az-204-dev/LAB/Lab07 - FunctionApp/v0.1/output.tf
+++ az-204-dev/LAB/Lab07 - FunctionApp/v0.1/output.tf
@@ -1,3 +1,7 @@
 output "url_func" {
   value = "https://${azurerm_windows_function_app.win_fun_app.default_hostname}"
 }
+
+output "sub_id" {
+  value = data.azurerm_subscription.sub.subscription_id
+}
